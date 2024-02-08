a9b0e8ca75de17006c8420eb5a0b9c037aed8a96
ahmedabdelhaliem <144239916+ahmedabdelhaliem@users.noreply.github.com>
2024-02-08 07:35:31 -0800

    first commit

---
+++ lib/feature/views/Splash_view.dart
@@ -0,0 +1,13 @@
+import 'package:booklyapp/feature/views/widgets/Splash_View_Body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
